0d616964fb87a2c5cdcac3cfc514a3c63ebb3a13
ADARBEH <retaad99@gmail.com>
2022-09-10 15:52:29 +0300

    done

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+
+const corsOptions ={
+    origin:'http://localhost:3001', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
+
 
 app.get('/' , (req,res)=>{
     res.status(200).send('Hello World')
 })
 
 app.post('/person',(req,res)=>{
-    res.status(200).json({
+    res.status(200).send({
         persons_name:req.body.name,
-        persons_age:req.body.age + 5 ,
+        persons_age:Number(req.body.age) + 5 ,
         persons_gender:req.body.gender
     })
 })
